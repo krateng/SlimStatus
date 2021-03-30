abc69598f977de6f3c26c008fefb2d3ccaa5aab2
krateng <git.noreply@krateng.ch>
2021-03-30 18:43:36 +0200

    Created python script

--- README.md
+++ README.md
@@ -1,41 +1,11 @@
-# Tinystatus
+# SlimStatus
 
-tinystatus generate an html status page via shell script.
+SlimStatus is a fork of the fantastic [tinystatus](https://github.com/bderenzo/tinystatus) by bderenzo, but slightly less tiny (hence the name).
 
-## Features
-
-* Parallel checks
-* HTTP, ping, port checks
-* HTTP expected status code (401, ...)
-* Minimal dependencies (curl, nc and coreutils)
-* Easy configuration and customisation
-* Incident history (manual)
-
-## Demo
-
-An example site is available [here](https://lab.bdro.fr/tinystatus/).
 
 ## Setup
 
-To install tinystatus:
-
 * Clone the repository and go to the created directory
-* Edit the checks file `checks.list`
-* To add incidents or maintenance, edit `incidents.list`
-* Generate status page `./tinystatus > index.html`
+* Edit the config file `config.yml`
+* Generate status page `python3 slimstatus.py`
 * Serve the page with your favorite web server
-
-## Configuration file
-
-The syntax of `checks.list` file is:
-```
-Command | Expected Code | Status Text | Host to check
-```
-
-Command can be:
-* `http` - Check http status
-* `ping` - Check ping status 
-* `port` - Check open port status
-
-There are also `http4`, `http6`, `ping4`, `ping6`, `port4`, `port6` for IPv4 or IPv6 only check.
-
---
+++ slimstatus.py
@@ -0,0 +1,130 @@
+import yaml
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from datetime import datetime
+import os
+from threading import Thread
+import requests
+import socket
+
+
+### jinja setup
+env = Environment(
+    loader=FileSystemLoader('./'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+template = env.get_template('page.jinja')
+
+
+
+### load config
+with open('config.yml') as f:
+	data = yaml.safe_load(f)
+
+
+
+
+###TESTS
+def test_ping(target,ipv,_):
+	result = os.system(f"ping -{ipv} -c 1 {target} > /dev/null")
+	return {
+		0: True,
+		1: "Ping failed",
+		256: "Host unreachable",
+		512: "Name not resolved"
+	}[result]
+		
+	
+	
+def test_https(target,ipv,port):
+	r = requests.get("https://" + target)
+	result = r.status_code
+	return {
+		200: True,
+		403: "Authentication Failure",
+		404: "Not found"
+	}[result]
+
+
+def test_port(target,ipv,port):
+	af = socket.AF_INET6 if ipv==6 else socket.AF_INET
+	with socket.socket(af, socket.SOCK_STREAM) as sock:
+		result = sock.connect_ex((target,port))
+	return {
+		0: True,
+		101: "Unknown Name",
+		111: "Wat"
+	}[result]
+
+tests = {
+	"port":test_port,
+	"https":test_https
+}
+
+### threaded op to perform and write results
+def perform_test(test,info,ipv,testarg=None):
+	target = info.get("target")
+	if f"target_{ipv}" in info: target = info.get(f"target_{ipv}")
+	try:
+		result = test(target,ipv,testarg)
+	except:
+		result = "Unknown Error"
+	if result is not True:
+		info['errors'].append(f"IPv{ipv}: {result}")
+		info[f'operational_{ipv}'] = False
+
+
+
+threads = []
+
+### perform all tests
+for service in data['services']:
+	#service['operational'] = True
+	service['errors'] = []
+	
+	
+	for ipv in [4,6]:
+		service[f'operational_{ipv}'] = None
+		if "target" in service or f"target_{ipv}" in service:
+			service[f'operational_{ipv}'] = True
+			for testtype,value in service['tests'].items():
+				test = tests[testtype]
+				t = Thread(target=perform_test,args=(test,service,ipv),kwargs={'testarg':value})
+				threads.append(t)
+				t.start()
+	
+	
+for machine in data['machines']:
+	#machine['operational'] = True
+	machine['errors'] = []
+
+	for ipv in [4,6]:
+		machine[f'operational_{ipv}'] = None
+		if "target" in machine or f"target_{ipv}" in machine:
+			machine[f'operational_{ipv}'] = True
+			for test in [test_ping]:
+				t = Thread(target=perform_test,args=(test,machine,ipv),kwargs={})
+				threads.append(t)
+				t.start()
+
+
+### wait until done	
+for t in threads:
+	t.join()
+
+
+
+for r in data['services'] + data['machines']:
+	r['operational'] = (r['operational_4'] in (True,None) and r['operational_6'] in (True,None))
+
+
+
+### extra information
+data['meta'] = {'date':datetime.utcnow().strftime("%Y/%m/%d %H:%M:%S")}
+
+
+
+
+### write result
+with open("page.html","w") as output:
+	output.write(template.render(data))
+
